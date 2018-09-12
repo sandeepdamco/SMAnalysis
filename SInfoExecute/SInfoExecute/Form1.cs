9c0cb648eff6d5a3572d2b658e4637ba1505f787
Sandeep <Sandeep1@damcogroup.com>
2018-09-12 16:49:56 +0530

    Window form and other analysis

---
+++ SInfoExecute/SInfoExecute/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SInfoExecute
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Timer MyTimer = new Timer();
+            MyTimer.Interval = (5 * 1000); // 45 mins
+            MyTimer.Tick += new EventHandler(MyTimer_Tick);
+            MyTimer.Start();
+        }
+        DataSet oldDs = new DataSet();
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection("Data Source=WKS-337\\SQLEXPRESS;Initial Catalog=SM;User ID =sa; Password =damco"))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_GetInfo", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@duration", SqlDbType.Int).Value = 3;
+                    con.Open();
+                    using (SqlDataAdapter adap = new SqlDataAdapter(cmd))
+                    {
+                        adap.Fill(ds);
+                    }
+                }
+            }
+            dataGridView1.DataSource = ds.Tables[0];
+            if (ds.Tables[0].Rows.Count > 0 && oldDs.Tables.Count > 0 && oldDs.Tables[0].Rows.Count != ds.Tables[0].Rows.Count)
+            {
+                this.BringToFront();
+            }
+            oldDs = ds;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyTimer_Tick(null, null);
+        }
+    }
+}
---
+++ SMAnalysis/SMAnalysis/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMAnalysis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Timer MyTimer = new Timer();
+            MyTimer.Interval = (45 * 60 * 1000); // 45 mins
+            MyTimer.Tick += new EventHandler(MyTimer_Tick);
+            MyTimer.Start();
+        }
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("The form will now be closed.", "Time Elapsed");
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file = @"D:\Project\SM\SMAnalysis\SMAnalysis\CIPLA.txt";
+            var lines = File.ReadAllLines(file);
+
+            foreach (var line in lines)
+            {
+                SmInfo smInfo = new SmInfo();
+                string[] columns = line.Split(",".ToCharArray());
+                smInfo.DateTime = GetDate(columns[1], columns[2]);
+                smInfo.OP = Convert.ToDecimal(columns[3]);
+                smInfo.HP = Convert.ToDecimal(columns[4]);
+                smInfo.LP = Convert.ToDecimal(columns[5]);
+                smInfo.CP = Convert.ToDecimal(columns[6]);
+                smInfo.volume = Convert.ToInt32(columns[7]);
+                ConstantHelper.ListRaw.Add(smInfo);
+            }
+
+            var list = ConstantHelper.ListRaw.Select(p => p.Date).Distinct();
+            foreach (var date in list)
+            {
+                var records = ConstantHelper.ListRaw.Where(p => p.Date == date);
+                SmDailyInfo smInfo = new SmDailyInfo();
+                smInfo.DateTime = date;
+                smInfo.OP = records.First().OP;
+                smInfo.HP = records.Max(p => p.HP);
+                smInfo.LP = records.Max(p => p.HP);
+                smInfo.CP = records.Last().CP;
+                smInfo.volume = records.Sum(p => p.volume);
+                ConstantHelper.DailyList.Add(smInfo);
+            }
+            dataGridView1.DataSource = ConstantHelper.DailyList;
+        }
+
+        private DateTime GetDate(string date, string time)
+        {
+            int year = Convert.ToInt32(date.Substring(0, 4));
+            int month = Convert.ToInt32(date.Substring(4, 2));
+            int day = Convert.ToInt32(date.Substring(6, 2));
+            int hours = Convert.ToInt32(time.Substring(0, 2));
+            int minute = Convert.ToInt32(time.Substring(3, 2));
+            DateTime dt = new DateTime(year, month, day, hours, minute, 0);
+            return dt;
+        }
+    }
+
+    public class SmInfo
+    {
+        public decimal LP { get; set; }
+
+        public decimal HP { get; set; }
+
+        public decimal OP { get; set; }
+
+        public decimal CP { get; set; }
+
+        public int volume { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public DateTime Date
+        {
+            get
+            {
+
+                return this.DateTime.Date;
+            }
+        }
+
+        public TimeSpan Time
+        {
+            get
+            {
+
+                return this.DateTime.TimeOfDay;
+            }
+        }
+    }
+
+
+    public class SmDailyInfo
+    {
+        public decimal LP { get; set; }
+
+        public decimal HP { get; set; }
+
+        public decimal OP { get; set; }
+
+        public decimal CP { get; set; }
+
+        public int volume { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public DateTime Date
+        {
+            get
+            {
+
+                return this.DateTime.Date;
+            }
+        }
+    }
+
+    public class ConstantHelper
+    {
+        public static List<SmInfo> ListRaw = new List<SmInfo>();
+
+        public static List<SmDailyInfo> DailyList = new List<SmDailyInfo>();
+
+        public static DateTime LastSynTime = DateTime.Today;
+    }
+
+}
