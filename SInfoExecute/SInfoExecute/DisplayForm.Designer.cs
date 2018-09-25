b31b71215df5f100b7ea399b572f560e508cb47a
Sandeep <Sandeep1@damcogroup.com>
2018-09-25 15:38:40 +0530

    Display form changes.

---
+++ SInfoExecute/SInfoExecute/DisplayForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SInfoExecute
+{
+    partial class DisplayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(12, -8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 54);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(412, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "H";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 69);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(353, 49);
+            this.panel2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // DisplayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 130);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DisplayForm";
+            this.ShowInTaskbar = false;
+            this.Text = "DisplayForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ SInfoExecute/SInfoExecute/DisplayForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SInfoExecute
+{
+    public partial class DisplayForm : Form
+    {
+        Timer MyTimer = new Timer();
+        public DisplayForm()
+        {
+            InitializeComponent();
+            MyTimer.Interval = 1000;
+            MyTimer.Tick += new EventHandler(MyTimer_Tick);
+            MyTimer.Start();
+            LoadDropDown();
+        }
+
+        public List<string> Names = new List<string>();
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0 || comboBox1.SelectedIndex == -1)
+                return;
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection("Data Source=WKS-337\\SQLEXPRESS;Initial Catalog=SM;User ID =sa; Password =damco"))
+            {
+                using (SqlCommand cmd = new SqlCommand("select top 1 lastPrice from sinfo where name ='" + comboBox1.SelectedValue + "' order by AddedDate desc ", con))
+                {
+                    con.Open();
+                    using (SqlDataAdapter adap = new SqlDataAdapter(cmd))
+                    {
+                        adap.Fill(ds);
+                    }
+                }
+            }
+            label1.Text = ds.Tables[0].Rows[0][0].ToString();
+        }
+
+        private void LoadDropDown()
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection("Data Source=WKS-337\\SQLEXPRESS;Initial Catalog=SM;User ID =sa; Password =damco"))
+            {
+                using (SqlCommand cmd = new SqlCommand("select distinct name from sinfo order by name", con))
+                {
+                    con.Open();
+                    using (SqlDataAdapter adap = new SqlDataAdapter(cmd))
+                    {
+                        adap.Fill(ds);
+                    }
+                }
+            }
+            comboBox1.DataSource = ds.Tables[0];
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Name";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            if (panel1.Visible)
+            {
+                panel1.Hide();
+                panel2.Show();
+            }
+            else
+            {
+                panel1.Show();
+                panel2.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
